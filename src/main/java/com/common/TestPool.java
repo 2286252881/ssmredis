dc2a3e2ccb60a3c390cfd25c072fe11510763ecf
2286252881 <2286252881@qq.com>
2016-07-12 09:56:03 +0800

    增加jedis工具类

---
+++ src/main/java/com/common/TestPool.java
@@ -0,0 +1,24 @@
+package com.common;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+public class TestPool {
+
+	public static void main(String[] args) {
+		JedisPool jedisPool = JedisPoolUtil.getJedisPoolInstance();
+		JedisPool jedisPool2 = JedisPoolUtil.getJedisPoolInstance();
+		
+		System.out.println(jedisPool == jedisPool2);
+		
+		Jedis jedis = null;
+		try {
+			jedis = jedisPool.getResource();
+			jedis.set("aa","bb");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			JedisPoolUtil.release(jedisPool, jedis);
+		}
+	}
+}
